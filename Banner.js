3d0dce41c88af3dfad7264a11df9c86087a2e64c
sabbondanza <90727020+sabbondanza@users.noreply.github.com>
2021-09-19 22:56:29 +0200

    Add files via upload

---
+++ Banner.js
@@ -0,0 +1,14 @@
+import logo from '../assets/newlogo.png'
+import '../styles/Banner.css'
+
+function Banner() {
+	const title = 'Dashboard PoS'
+	return (
+		<div className='lmj-banner'>
+			<img src={logo} alt='Dashboard PoS' className='lmj-logo' />
+			<h1 className='lmj-title'>{title}</h1>
+		</div>
+	)
+}
+
+export default Banner
---
+++ PlantList.js
@@ -0,0 +1,55 @@
+export const plantList = [
+	{
+		name: 'monstera',
+		category: 'classique',
+		id: '1ed',
+		isBestSale: true
+	},
+	{
+		name: 'ficus lyrata',
+		category: 'classique',
+		id: '2ab',
+		isBestSale: true
+	},
+	{
+		name: 'pothos argenté',
+		category: 'classique',
+		id: '3sd',
+		isBestSale: false
+	},
+	{
+		name: 'yucca',
+		category: 'classique',
+		id: '4kk',
+		isBestSale: false
+	},
+	{
+		name: 'olivier',
+		category: 'extérieur',
+		id: '5pl'
+	},
+	{
+		name: 'géranium',
+		category: 'extérieur',
+		id: '6uo',
+		isBestSale: true
+	},
+	{
+		name: 'basilique',
+		category: 'extérieur',
+		id: '7ie',
+		isBestSale: false
+	},
+	{
+		name: 'aloe',
+		category: 'plante grasse',
+		id: '8fp',
+		isBestSale: true
+	},
+	{
+		name: 'succulente',
+		category: 'plante grasse',
+		id: '9vn',
+		isBestSale: false
+	}
+]
